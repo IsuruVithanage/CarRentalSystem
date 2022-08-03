cbf90feeabdb0409369a972c1e74b0a466f97c67
IsuruVithanage <isuruvithanagemv@gmail.com>
2022-08-03 18:49:59 +0530

    Admin chart complete

--- FrontEnd/controllers/ChartController.js
+++ FrontEnd/controllers/ChartController.js
@@ -1,47 +1,63 @@
-var xValues = ["January", "February", "March", "April", "June", "July", "August","September","October","November","December"];
-var a=0;
-getIncome("08");
-var yValues = [50000, 60000, 30000, 70000, 95000, 47000, a, 11, 14, 14, 15];
-
-new Chart("myChart", {
-    type: "line",
-    data: {
-        labels: xValues,
-        datasets: [{
-            fill: false,
-            lineTension: 0,
-            backgroundColor: "rgb(255,140,0)",
-            borderColor: "rgba(0,0,255,0.1)",
-            data: yValues
-        }]
-    },
-    options: {
-        legend: {display: false},
-        scales: {
-            yAxes: [{ticks: {min: 8000, max: 150000}}],
-        }
-    }
-});
 
 
-function getIncome(month) {
+
+function getIncome() {
     $.ajax({
         url: "http://localhost:8080/CarRentalSystem_war/payment",
         method: "GET",
         success: function (resp) {
             var d = new Date();
-            var output = d.getFullYear() + '-' + month + '-';
-            var tot=0;
+            var ma = d.getFullYear() + '-' + "08" + '-';
+            var ms = d.getFullYear() + '-' + "09" + '-';
+            var mo = d.getFullYear() + '-' + "10" + '-';
+            var mn = d.getFullYear() + '-' + "11" + '-';
+            var md = d.getFullYear() + '-' + "12" + '-';
+            var totA=0;
+            var totS=0;
+            var totO=0;
+            var totN=0;
+            var totD=0;
             for (const respElement of resp.data) {
                 var str=respElement.date;
-                console.log(str.indexOf(output));
-                if (str.indexOf(output) >= 0){
-                    tot=tot+parseFloat(respElement.price);
+                if (str.indexOf(ma) >= 0){
+                    totA=totA+parseFloat(respElement.price);
+                }
+                if (str.indexOf(ms) >= 0){
+                    totS=totS+parseFloat(respElement.price);
+                }
+                if (str.indexOf(mo) >= 0){
+                    totO=totO+parseFloat(respElement.price);
+                }
+                if (str.indexOf(mn) >= 0){
+                    totN=totN+parseFloat(respElement.price);
+                }
+                if (str.indexOf(md) >= 0){
+                    totD=totD+parseFloat(respElement.price);
                 }
 
             }
-            //console.log(tot);
-            a=tot;
+            var xValues = ["January", "February", "March", "April", "June", "July", "August","September","October","November","December"];
+            var yValues = [50000, 60000, 30000, 70000, 95000, 47000, totA, totS, totO, totN, totD];
+
+            new Chart("myChart", {
+                type: "line",
+                data: {
+                    labels: xValues,
+                    datasets: [{
+                        fill: false,
+                        lineTension: 0,
+                        backgroundColor: "rgb(255,140,0)",
+                        borderColor: "rgba(0,0,255,0.1)",
+                        data: yValues
+                    }]
+                },
+                options: {
+                    legend: {display: false},
+                    scales: {
+                        yAxes: [{ticks: {min: 8000, max: 150000}}],
+                    }
+                }
+            });
 
         },
         error: function (ob) {
